70d99577d546c547e535a9e248ee4738feb2b502
WIN-O6LHRQF520R\Admin <keengrost@gmail.com>
2020-10-16 10:17:14 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
